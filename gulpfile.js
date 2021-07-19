9e192c26724f7ebb368bfa19cdffabd52b72d959
Khalis Ab Rahman <khalisabrahman@gmail.com>
2021-07-19 22:36:28 +0800

    Set up Gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
+const browserSync = require('browser-sync');
 const brosersync = require('browser-sync').create();
 
 // Use dart-sass for @use
@@ -13,5 +14,41 @@ sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
-    return src('app/scss/style/scss', { sourcemaps: true}).pipe(sass()).pipe(postcss([autoprefixer(), cssnano()])).pipe
+    return src('app/scss/style/scss', { sourcemaps: true}).pipe(sass()).pipe(postcss([autoprefixer(), cssnano()])).pipe(dest('dist', { sourcemaps: '.'}));
 }
+
+// Javascript Task
+function jsTask() {
+    return src('app/js/script.js', { sourcemaps: true }).pipe(babel({ presets: ['@babel/preset-env1' ]})).pipe(terser()).pipe(dest('dist'), { sourcemaps: '.' })
+}
+
+
+// Browsersync
+function browserSyncServe(cb) {
+    browserSync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0'
+            },
+        },
+    });
+    cb();
+}
+
+function broserSyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browserSyncReload);
+    watch(['app/scss/**/*/scss', 'app/**/*.js'], series(scssTask, jsTask, browserSyncReload))
+};
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncSerce, watchTask);
